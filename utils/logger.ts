325c41a098791712723e14ead6f2ab75df3588f3
Florian Pürschel <hello@prshl.de>
2021-11-09 20:20:26 +0100

    Update logger.ts

--- utils/logger.ts
+++ utils/logger.ts
@@ -4,13 +4,16 @@ class Logger {
 	clear = () => console.clear();
 
 	info = (_information: string): void =>
-		console.log(Colors.cyan, 'INFO:\t', Colors.white, `${_information}`);
+		console.log(Colors.cyan, 'INFO:\t\t', Colors.white, `${_information}`);
 
 	warn = (_warning: string): void =>
 		console.log(Colors.yellow, 'WARNING:\t', Colors.white, `${_warning}`);
 
 	error = (_error: string): void =>
-		console.log(Colors.red, 'WARNING:\t', Colors.white, `${_error}`);
+		console.log(Colors.red, 'ERROR:\t\t', Colors.white, `${_error}`);
+
+	success = (_success: string): void =>
+		console.log(Colors.green, 'DONE:\t\t', Colors.white, `${_success}`);
 }
 
 const logger = new Logger();
