e3847c19c07a8e82c10ab39703cb3f4894b24553
Florian Pürschel <hello@prshl.de>
2021-11-09 20:21:50 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+/**
+ * The WebScraper Project was created to collect files from GitHub without
+ * getting stuck in the GitHub-API-Rate-Limits.
+ * @author Florian Pürschel
+ * @version 1.0.0
+ * @url www.prshl.de
+ */
+
 import Collector from './collector';
 import Browser from './browser';
 import Logger from './utils/logger';
@@ -7,10 +15,15 @@ import Logger from './utils/logger';
  * to gather files with a given file-extension.
  */
 const main = async () => {
+	Logger.clear();
 	Logger.info('Starting the file-collector.');
 
-	const browser = new Browser(false);
+	const browser = new Browser();
 	const page = await browser.initialize();
+
+	const collector = new Collector(page);
+
+	await collector.collect();
 
 	Logger.info('Finishing the the file-collector.');
 	return browser.close();
